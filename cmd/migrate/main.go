bdf972582322cfe56b5b92d937a1d905232341a0
Markus Wüstenberg <markus@maragu.dk>
2022-11-03 13:31:49 +0100

    Add migrate command (#7)

---
+++ cmd/migrate/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/maragudk/env"
+	"github.com/maragudk/migrate"
+
+	"github.com/maragudk/sqlite-app/sql"
+)
+
+func main() {
+	log := log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
+
+	_ = env.Load()
+
+	db := sql.NewDatabase(sql.NewDatabaseOptions{
+		Log:                log,
+		URL:                env.GetStringOrDefault("DATABASE_URL", "file:app.db"),
+		MaxOpenConnections: 1,
+		MaxIdleConnections: 1,
+	})
+
+	if err := db.Connect(); err != nil {
+		log.Fatalln("Error connecting to database:", err)
+	}
+
+	fsys := os.DirFS("sql/migrations")
+
+	if len(os.Args) < 2 {
+		log.Fatalln("up or down?")
+	}
+
+	switch os.Args[1] {
+	case "up":
+		if err := migrate.Up(context.Background(), db.DB.DB, fsys); err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("Migrated up")
+	case "down":
+		if err := migrate.Down(context.Background(), db.DB.DB, fsys); err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("Migrated down")
+	default:
+		log.Fatalln("unknown command " + os.Args[1])
+	}
+}
