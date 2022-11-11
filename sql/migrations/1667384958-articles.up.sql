45280179939880943cce57b8505726cbb84bd3f6
Markus Wüstenberg <markus@maragu.dk>
2022-11-11 09:58:19 +0100

    Use millisecond precision and UTC for timestamps
    Because e.g. `2022-11-11T12:34:56.000Z` sorts _after_ `2022-11-11T12:34:56.000000001Z`, restrict the timestamps used with SQLite to millisecond precision. This is because the built-in `strftime` of SQLite only supports millisecond precision, otherwise we could have switched to nanosecond precision everywhere.
    
    An alternative is to not use the `strftime` function to create timestamps, but then we lose the ability to set timestamps on e.g. update triggers.
    
    Also, always use UTC when (de)serializing `model.Time`.
    
    Fixes #12.

--- sql/migrations/1667384958-articles.up.sql
+++ sql/migrations/1667384958-articles.up.sql
@@ -2,12 +2,12 @@ create table articles (
   id integer primary key,
   title text not null,
   content text not null,
-  created text not null default (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')),
-  updated text not null default (strftime('%Y-%m-%dT%H:%M:%fZ', 'now'))
+  created text not null default (strftime('%Y-%m-%dT%H:%M:%fZ')),
+  updated text not null default (strftime('%Y-%m-%dT%H:%M:%fZ'))
 ) strict;
 
 create index articles_created_idx on articles (created);
 
 create trigger articles_updated_timestamp after update on articles begin
-  update articles set updated = (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')) where id = old.id;
+  update articles set updated = (strftime('%Y-%m-%dT%H:%M:%fZ')) where id = old.id;
 end;
